94b2987234fdd8cc3d7a4cd0c85339741fec884d
dschwartz783 <dschwartz783@gmail.com>
2016-07-26 06:29:53 -0400

    initial commit

---
+++ src/DDSPlugins/PMInvisibleBedrockRemover/PMInvisibleBedrockRemover.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: funtimes
+ * Date: 6/19/16
+ * Time: 8:08 AM
+ */
+
+namespace DDSPlugins\PMInvisibleBedrockRemover;
+
+use pocketmine\block\Block;
+use pocketmine\entity\Entity;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\item\Item;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\entity\Effect;
+
+class PMInvisibleBedrockRemover extends PluginBase
+{
+    function onEnable()
+    {
+        $this->getLogger()->info("PMInvisibleBedrockRemover Enabled");
+    }
+
+    function onCommand(CommandSender $sender, Command $command, $label, array $args)
+    {
+        if ($sender instanceof Player) {
+            $player_position = $sender->getPosition();
+            $block_position = $player_position->add(0, -1, 0);
+            if ($sender->getLevel()->getBlock($block_position)->getId() == Block::INVISIBLE_BEDROCK){
+                $sender->getLevel()->setBlock($block_position, Block::get(Block::AIR));
+            }
+        }
+        return true;
+    }
+
+    function console_error_message() {
+        $this->getLogger()->error("This command cannot be run from console");
+        return true;
+    }
+}
